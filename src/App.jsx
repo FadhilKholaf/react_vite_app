2a838c5a0d7e875b973edd76b53c13ea49e4b491
FadhilKholaf <fadhilgd123@gmail.com>
2023-11-11 22:59:59 +0700

    vite tool for react

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          APA COBA 😂😂😂
         </p>
       </div>
       <p className="read-the-docs">
